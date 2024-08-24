e83ab22b269e26bc0134f62bc54b638641f4fe7b
Chandan <chandanv925@gmail.com>
2024-08-24 20:19:37 +0530

    Test

--- C_problems/DSA/00_array_sorting.c
+++ C_problems/DSA/00_array_sorting.c
@@ -2,12 +2,12 @@
 #include <stdlib.h>
 
 /*
-Sorting Algorithm    Best Case        Average Case	  Worst Case	 Space Complexity
-Selection Sort        O(n²)            O(n²)	        O(n²)	        O(1)
-Insertion Sort        O(n)             O(n²)	        O(n²)	        O(1)
-Quick Sort            O(n log n)	   O(n log n)	    O(n²)	        O(log n)
-Merge Sort            O(n log n)	   O(n log n)	    O(n log n)	    O(n)
-Heap Sort             O(n log n)	   O(n log n)	    O(n log n)	    O(1)
+Sorting Algorithm       Best Case       Average Case    Worst Case      Space Complexity
+Selection Sort          O(n²)           O(n²)           O(n²)           O(1)
+Insertion Sort          O(n)            O(n²)           O(n²)           O(1)
+Quick Sort              O(n log n)      O(n log n)      O(n²)           O(log n)
+Merge Sort              O(n log n)      O(n log n)      O(n log n)      O(n)
+Heap Sort               O(n log n)      O(n log n)      O(n log n)      O(1)
 */
 
 // Create a structure of an array
